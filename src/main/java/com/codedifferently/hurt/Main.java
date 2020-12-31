3f7c9d99ebd99b4323eac3d80244a50a8c35ab6f
Faith Wilkins El <fwilkins@terpmail.umd.edu>
2020-12-31 18:47:48 -0500

    Empty shell for examples

--- src/main/java/com/codedifferently/hurt/Main.java
+++ src/main/java/com/codedifferently/hurt/Main.java
@@ -1,8 +1,18 @@
 package com.codedifferently.hurt;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import org.apache.commons.io.IOUtils;
 
 public class Main {
+    //how are we going to store the products?  LinkedHashMap
+    static Map<String , Map<String, Integer>> products = new LinkedHashMap<>();
+
+    //how are we going to keep track of error? We need to keep track of initial length and final of the array
+    static int initialSze = 0;
+    static int finalSze = 0;
 
     public String readRawDataToString() throws Exception{
         ClassLoader classLoader = getClass().getClassLoader();
@@ -13,6 +23,74 @@ public class Main {
     public static void main(String[] args) throws Exception{
         String output = (new Main()).readRawDataToString();
         System.out.println(output);
+        //here we need an arrayList to use and where we will do the operations
+    }
+
+    /* First method: Turn the product string into an arrayList seperated 
+      by the delimiter [^. @....etc] */
+
+    public static ArrayList<String> convertToArray(String str){
+        //What do we need to do to the String before we operate on it?
+        //Remove the special characters except for the # , . and / (why? we need these
+        // to determine the price and delimination and date) 
+        //Replace them with a " "
+
+        //Convert everything into lowerCase
+
+        //Intialize return arrayList as a list splited by the ## 
+
+
+        //Operate on the arrayList so that we remove the "name" signature from each element
+
+
+        //set the intialSze to the size of the arrayList (This will be the true count of products)
+
+
+        //return the arrayList
+    }
+
+    /* Second method:Remove the list elements that are not starting with "price" This denotes an 
+    error */
+
+    public static ArrayList<String> trimList(ArrayList<String> arr){
+        //Loop through each of the elements in arr
+
+             //In each list element, create a string array split by the " " 
+            //(This separates the element into smaller elements like price, type, expiration etc)
+         
+                //if the first element in the String array is not equal tp price remove it
+
+                //else if the third element in the String array doesn't have a number but says "type" then remove it
+
+
+        //set the finalSze to the size of the new list
+
+        //return the list
+    }
+
+
+    /*Third method: Put the information into a LinkedHashmap, this will help us count everything */
+    public static void populateLinkedMap(ArrayList<String> arr){
+        //loop through each string in the list
+
+           //split the list into categories 
+           //Then check the c00kie input replace all names that have 0 with o
+
+           //add the elements to the map
+           // Map: 
+                //category -> 
+                           //3rd category (price)  : count 
+                           //3rd category (price)  : count
+        /* Example of what a String arr list element looks like: 
+            arr[0] = "Co0kieS pRice 2.25"
+        */
 
     }
+
+    /* Fourth method: format output to match the output text*/
+    public static void formatOutput(){
+
+    }
+
+
 }
--- src/test/java/com/codedifferently/hurt/MainTest.java
+++ src/test/java/com/codedifferently/hurt/MainTest.java
@@ -1,4 +1,90 @@
 package com.codedifferently.hurt;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Map;
+
 public class MainTest {
+    //static String sampleString = "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##" +
+           // "naME:BreaD;price:1.23;type:Food;expiration:1/02/2016";
+    //static String sampleString2 = sampleString + "##NAMe:BrEAD;price:1.23;type:Food;expiration:2/25/2016##" +
+          //  "naMe:MiLK;price:3.23;type:Food^expiration:1/11/2016";
+
+    @Test
+    public void convertToArrayTest() {
+        String sampleString = "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##" +
+                "naME:BreaD;price:1.23;type:Food;expiration:1/02/2016";
+        int expectedElements = 2;
+
+        assertEquals(expectedElements, Main.convertToArray(sampleString).size());
+    }
+
+    @Test
+    public void convertToArrayTest2() {
+        String sampleString2 = "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##" +
+                "naME:BreaD;price:1.23;type:Food;expiration:1/02/2016##" +
+                "NAMe:BrEAD;price:1.23;type:Food;expiration:2/25/2016##" +
+                "naMe:MiLK;price:3.23;type:Food^expiration:1/11/2016";
+
+        int expectedElements = 4;
+
+        assertEquals(expectedElements, Main.convertToArray(sampleString2).size());
+    }
+
+    @Test
+    public void trimListTest() {
+        String fourElements = "naMe:MiLK;priCe:;type:Food;expiration:1/11/2016##" +
+                "naMe:Cookies;price:2.25;type:Food;expiration:1/25/2016##" +
+                "naMe:Co0kieS;pRice:2.25;type:Food;expiration:1/25/2016##" +
+                "naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016";
+
+        int expectedElements = 3;
+
+        assertEquals(expectedElements, Main.trimList(Main.convertToArray(fourElements)).size());
+    }
+
+    @Test
+    public void trimListTest2() {
+        String nineElements = "naMe:Cookies;price:2.25;type:Food;expiration:1/25/2016##" +
+                "naMe:Co0kieS;pRice:2.25;type:Food;expiration:1/25/2016##" +
+                "naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016##" +
+                "naMe:COOkieS;Price:2.25;type:Food;expiration:1/25/2016##" +
+                "NAME:MilK;price:3.23;type:Food;expiration:1/17/2016##" +
+                "naMe:MilK;priCe:;type:Food;expiration:4/25/2016##" +
+                "naMe:apPles;prIce:0.25;type:Food;expiration:1/23/2016##" +
+                "naMe:apPles;pRice:0.23;type:Food;expiration:5/02/2016##" +
+                "NAMe:BrEAD;price:1.23;type:Food;expiration:1/25/2016##" +
+                "naMe:;price:3.23;type:Food^expiration:1/04/2016";
+
+        int expectedElements = 8;
+
+        assertEquals(expectedElements, Main.trimList(Main.convertToArray(nineElements)).size());
+    }
+
+    @Test
+    public void populateLinkedMapTest() {
+        ArrayList<String> bigString = Main.trimList(Main.convertToArray
+                ("naMe:Cookies;price:2.25;type:Food;expiration:1/25/2016##" +
+                "naMe:Co0kieS;pRice:2.25;type:Food;expiration:1/25/2016##" +
+                "naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016##" +
+                "naMe:COOkieS;Price:2.25;type:Food;expiration:1/25/2016##" +
+                "NAME:MilK;price:3.23;type:Food;expiration:1/17/2016##" +
+                "naMe:MilK;priCe:;type:Food;expiration:4/25/2016##" +
+                "naMe:apPles;prIce:0.25;type:Food;expiration:1/23/2016##" +
+                "naMe:apPles;pRice:0.23;type:Food;expiration:5/02/2016##" +
+                "NAMe:BrEAD;price:1.23;type:Food;expiration:1/25/2016##" +
+                "naMe:;price:3.23;type:Food^expiration:1/04/2016"));
+
+        int expectedCookiesSeen = 4;
+        Main.populateLinkedMap(bigString);
+        int seenCount = 0;
+        System.out.println(Main.products.values());
+        for (Map.Entry<String, Integer> entry : Main.products.get("cookies").entrySet()) {
+            seenCount += entry.getValue();
+        }
+
+        assertEquals(expectedCookiesSeen, seenCount);
+    }
 }
